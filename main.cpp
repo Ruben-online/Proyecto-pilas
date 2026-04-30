d87c3093edfd9ed6c1705266799b19a36b4bcbff
Fernando Espinoza <ruferesfer@gmail.com>
2026-04-30 09:37:28 -0600

    Adaptación del menú

--- main.cpp
+++ main.cpp
@@ -5,32 +5,36 @@ int main() {
     int option;
 
     do {
-        cout << "\n===== Menú principal =====" << endl;
-        cout << "1. Opción 1" << endl;
-        cout << "2. Opción 2" << endl;
-        cout << "3. Opción 3" << endl;
-        cout << "4. Salir" << endl;
-        cout << "Seleccione una opción: ";
+        cout << "\n===== PROYECTO PILAS =====" << endl;
+        cout << "1. Ingresar expresion infija" << endl;
+        cout << "2. Convertir a notacion postfija" << endl;
+        cout << "3. Evaluar expresion postfija" << endl;
+        cout << "4. Mostrar resultados" << endl;
+        cout << "5. Salir" << endl;
+        cout << "Seleccione una option: ";
         cin >> option;
 
         switch (option) {
             case 1:
-                cout << "Elegiste la opción 1" << endl;
+                cout << ">> Ingreso de expresion pendiente" << endl;
                 break;
             case 2:
-                cout << "Elegiste la opción 2" << endl;
+                cout << ">> Conversion a postfija pendiente" << endl;
                 break;
             case 3:
-                cout << "Elegiste la opción 3" << endl;
+                cout << ">> Evaluacion pendiente" << endl;
                 break;
             case 4:
+                cout << ">> Mostrar resultados pendiente" << endl;
+                break;
+            case 5:
                 cout << "Saliendo del programa..." << endl;
                 break;
             default:
-                cout << "Opción inválida" << endl;
+                cout << "option invalida" << endl;
         }
 
-    } while (option != 4);
+    } while (option != 5);
 
     return 0;
 }
