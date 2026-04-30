eed5e4a54b28893fff02d7be182c65674404098a
Fernando Espinoza <ruferesfer@gmail.com>
2026-04-30 11:44:39 -0600

    Se integra la pila al programa principal

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct nodopila {
+    char dato;
+    struct nodopila *enlace;
+};
+
+// Funciones
+void push(struct nodopila **top, char valor);
+char pop(struct nodopila **top);
+
 int main() {
-    int option;
+    int opcion;
+
+    // Variables 
+    string infija = "";
+    string postfija = "";
+
+    // Pila
+    struct nodopila *pila = NULL;
 
     do {
         cout << "\n===== PROYECTO PILAS =====" << endl;
@@ -11,30 +28,39 @@ int main() {
         cout << "3. Evaluar expresion postfija" << endl;
         cout << "4. Mostrar resultados" << endl;
         cout << "5. Salir" << endl;
-        cout << "Seleccione una option: ";
-        cin >> option;
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
 
-        switch (option) {
+        switch (opcion) {
             case 1:
-                cout << ">> Ingreso de expresion pendiente" << endl;
+                cout << "Ingrese la expresion infija: ";
+                cin.ignore();
+                getline(cin, infija);
+                cout << "Expresion guardada: " << infija << endl;
                 break;
+
             case 2:
                 cout << ">> Conversion a postfija pendiente" << endl;
                 break;
+
             case 3:
                 cout << ">> Evaluacion pendiente" << endl;
                 break;
+
             case 4:
-                cout << ">> Mostrar resultados pendiente" << endl;
+                cout << "Expresion infija: " << infija << endl;
+                cout << "Expresion postfija: " << postfija << endl;
                 break;
+
             case 5:
                 cout << "Saliendo del programa..." << endl;
                 break;
+
             default:
-                cout << "option invalida" << endl;
+                cout << "Opcion invalida" << endl;
         }
 
-    } while (option != 5);
+    } while (opcion != 5);
 
     return 0;
 }
