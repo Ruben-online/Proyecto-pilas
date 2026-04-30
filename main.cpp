02e2b25a3e27c292be784710ed89d061db4067f8
Fernando Espinoza <ruferesfer@gmail.com>
2026-04-30 09:20:37 -0600

    Creación de plantilla para menú en consola

--- main.cpp
+++ main.cpp
@@ -2,6 +2,35 @@
 using namespace std;
 
 int main() {
-    cout << "Creación del proyecto con git" << endl;
+    int option;
+
+    do {
+        cout << "\n===== Menú principal =====" << endl;
+        cout << "1. Opción 1" << endl;
+        cout << "2. Opción 2" << endl;
+        cout << "3. Opción 3" << endl;
+        cout << "4. Salir" << endl;
+        cout << "Seleccione una opción: ";
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                cout << "Elegiste la opción 1" << endl;
+                break;
+            case 2:
+                cout << "Elegiste la opción 2" << endl;
+                break;
+            case 3:
+                cout << "Elegiste la opción 3" << endl;
+                break;
+            case 4:
+                cout << "Saliendo del programa..." << endl;
+                break;
+            default:
+                cout << "Opción inválida" << endl;
+        }
+
+    } while (option != 4);
+
     return 0;
 }
